a613f0ed3f5ad1af52a7894f6f0f7abe3600a612
xiaowutonxue <jiazewu001@gmail.com>
2023-10-14 20:42:17 +0800

    feat:增加myMap

--- JavaScript-api/myMap.js
+++ JavaScript-api/myMap.js
@@ -1,3 +1,12 @@
+/*
+map(callbackFn)
+map(callbackFn, thisArg)
+
+fn传入element，index，arrar,
+thisArg
+返回一个新数组，每个元素都是回调函数的返回值
+*/
+
 Array.prototype.myMap = function(fn,context){
     var arr = Array.prototype.slice.call(this)
     var arrMap = []
@@ -5,4 +14,4 @@ Array.prototype.myMap = function(fn,context){
         arrMap.push(fn.call(context,arr[i],i,this))
     }
     return arrMap
-}
+} 
