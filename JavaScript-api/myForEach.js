1ad49c4d4686fbaa3b52109de22c7d20d73a8da0
jiazewu <jiazewu001@gmail.com>
2023-11-06 18:05:34 +0800

    feat:myforEach

---
+++ JavaScript-api/myForEach.js
@@ -0,0 +1,17 @@
+Array.prototype.myForEach = function (callback){
+    for(let i=0;i<this.length;i++){
+        callback(this[i],i,this)
+    }
+}
+
+const players = [
+    { name: '科比', num: 24 },
+    { name: '詹姆斯', num: 23 },
+    { name: '保罗', num: 3 },
+    { name: '威少', num: 0 },
+    { name: '杜兰特', num: 35 }
+]
+
+players.myForEach((item,index,arr)=>{
+    console.log(item,index,arr);
+})
