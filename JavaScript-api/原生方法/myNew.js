82b47877e38c3cefcedcc7fd65e472cdb1a2ab2b
jiazewu <jiazewu001@gmail.com>
2023-11-09 14:17:12 +0800

    feat:增加一种new的写法

--- JavaScript-api/原生方法/myNew.js
+++ JavaScript-api/原生方法/myNew.js
@@ -1,7 +1,7 @@
-function myNew (fn,...args){
+function myNew(fn, ...args) {
     let instance = Object.create(fn.prototype)
-    let res = fn.apply(instance,args)
-    return typeof res==='object'?res:instance
+    let res = fn.apply(instance, args)
+    return typeof res === 'object' ? res : instance
 }
 /*
 new一个函数的过程
@@ -10,3 +10,10 @@ new一个函数的过程
 3.执行构造函数
 4.如果返回值为object就返回这个对象，否则返回新对象
 */
+
+function myNew(fn, ...args) {
+    const obj = {}                                          // 1.创建一个全新的对象
+    obj._proto_ = fn.prototype                              // 2.这个全新的对象prototype指向构造函数的原型对象
+    let res = fn.apply(obj, args)                     // 3.执行构造函数
+    return typeof res === 'object' ? res : obj              // 4.如果返回值为object就返回这个对象，否则返回新对象
+}
